d9092911e325cc070b79f9cd0ac3f9ed60a0d2b5
Jdavp <juandiegoalejandro@hotmail.com>
2019-02-08 23:39:01 +0000

    all 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ *main - print all single digits of base 10
+ *
+ *print numbers
+ *
+ *Return: its 0
+ */
+int main(void)
+{
+	int onten;
+
+	for (onten = 0 ; onten <= 9 ; onten++)
+	{
+		printf("%d", onten);
+	}
+	printf("\n");
+	return (0);
+}
