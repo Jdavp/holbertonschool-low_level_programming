e354bd5c2ca9f5256b5dbaaf2cdffa4d08349c45
Jdavp <juandiegoalejandro@hotmail.com>
2019-02-08 20:53:29 +0000

    changes

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,3 +1,23 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
+
+/**
+ * main - print alphabet
+ *
+ *print alphabet
+ *
+ *Return: its 0
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+	{
+		if (ch != 'q' && ch != 'e')
+			putchar (ch);
+	}
+	putchar('\n');
+	return (0);
+}
