8151cbf7b081773535328d1fbc1e9fa93d49d9b0
Jdavp <juandiegoalejandro@hotmail.com>
2019-02-09 02:03:38 +0000

    first 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include<stdlib.h>
+#include<time.h>
+#include<stdio.h>
+
+/**
+ *main - print list of numbers
+ *
+ *numers with ,
+ *
+ *Return: 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 48 ; n <= 57 ; n++)
+	{
+		putchar(n);
+		if (n != 57)
+	{
+		putchar(',');
+		putchar(' ');
+	}
+	}
+	putchar('\n');
+	return (0);
+}
