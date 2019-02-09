15b5b34f3c29e259c340416d4606bc819bb6a166
Jdavp <juandiegoalejandro@hotmail.com>
2019-02-09 01:04:29 +0000

    first 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include<stdlib.h>
+#include<time.h>
+#include<stdio.h>
+
+/**
+ *main - prints all single digit numbers
+ *
+ *print number with new line
+ *
+ *Return: o
+ */
+int main(void)
+{
+	int onten;
+
+	for (onten = 48 ; onten <= 57 ; onten++)
+	{
+		putchar(onten);
+	}
+	for (onten = 97 ; onten <= 102 ; onten++)
+	{
+	putchar(onten);
+	}
+	putchar('\n');
+	return (0);
+}
