01f8a054b000ed9e62430717cfccd8555dcb39ee
Jdavp <juandiegoalejandro@hotmail.com>
2019-02-09 17:16:22 +0000

    first 10

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,30 @@
+#include<stdlib.h>
+#include<time.h>
+#include<stdio.h>
+
+/**
+ *main - print list of numbers
+ *
+ *numers with , a to 99
+ *
+ *Return: 0
+ */
+int main(void)
+{
+	int deci;
+	int digi;
+
+	for (deci = 48 ; deci <= 57 ; deci++)
+	{
+		for (digi = 48 ; digi <= 57 ; digi++)
+		{
+			putchar(deci);
+			putchar(digi);
+			if (!(deci == 57 && digi == 57))
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
