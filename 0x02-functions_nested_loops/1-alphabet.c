6fc990c6c333d1c2bfe2352d65c9797bb102efb8
Jdavp <juandiegoalejandro@hotmail.com>
2019-02-12 21:26:18 +0000

    first 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ *print alphabet
+ *
+ *print alphabet
+ *
+ *Return: void
+ */
+
+void print_alphabet(void)
+{
+	char ch;
+
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+	{
+	_putchar(ch);
+	}
+	_putchar('\n');
+}
