d17ea3d630da4645aa1ace95ab81305723eb2624
Jdavp <juandiegoalejandro@hotmail.com>
2019-02-12 19:03:25 +0000

    first 0

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * main - will print holberton
+ *
+ * Return: 0 success
+ *
+ */
+int main(void)
+
+{
+
+		_putchar('H');
+		_putchar('o');
+		_putchar('l');
+		_putchar('b');
+		_putchar('e');
+		_putchar('r');
+		_putchar('t');
+		_putchar('o');
+		_putchar('n');
+		_putchar('\n');
+
+	return (0);
+}
