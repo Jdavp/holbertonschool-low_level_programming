047c200c053f0e844e69783caa08ed06bf51f843
Jdavp <juandiegoalejandro@hotmail.com>
2019-02-13 03:58:35 +0000

    first 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * add - print only absolute numbers
+ *
+ *@a: - numbers entrance
+ *@b: - numbers entrance
+ *@total: - total
+ *
+ *Return: total
+ */
+
+int add(int a , int b)
+
+{
+	int total;
+
+	total = a + b;
+	return (total);
+}
