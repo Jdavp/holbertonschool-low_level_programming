a1c100881614039158911ea16c4e0b20e743b654
Jdavp <juandiegoalejandro@hotmail.com>
2019-02-14 13:39:44 +0000

    first 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+ * add - print only absolute numbers
+ *
+ *@l: - numbers entrance
+ *@c: - numbers entrance
+ *
+ */
+void times_table(void)
+{
+	int l;
+	int c;
+
+	for (l = 48 ; l <= 57 ; l++)
+	{
+		for ( c = 48 ; c <= 57 ; p ++)
+		{
+			p = (l x c);
+				_putchar(p);
+			{
+				_putchar(',');
+				_putchar('\n');
+			}
+		}
+	}
+}
