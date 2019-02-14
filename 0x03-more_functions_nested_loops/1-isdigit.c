a80b6b838af8cfc37cd2a701ba6c1ccac65dac23
Jdavp <jdanovi@gmail.com>
2019-02-14 22:29:09 +0000

    first 1

---
+++ 0x03-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * _isupper - check for  uppercase letters
+ *
+ *@c: - letters entrance
+ *
+ *Return: 1 its is upper case
+ */
+
+int _isdigit(int c)
+
+{
+  if (c >= 48  && c <= 57)
+    return (1);
+
+  else
+    return (0);
+}
