217c2732ab43f624b4fd4faf7e3f77c09f4cacc3
Jdavp <jdanovi@gmail.com>
2019-02-14 22:42:09 +0000

    first 2

---
+++ 0x03-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * mul - prints products numbers
+ *
+ *@a: - numbers entrance
+ *@b: - numbers entrance
+ *
+ *Return: total
+ */
+
+int mul(int a, int b)
+{
+int t;
+
+t = a * b;
+return (t);
+}
