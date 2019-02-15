020ac8a604345aad998b82cc38f8096fa876079f
Jdavp <jdanovi@gmail.com>
2019-02-15 03:31:41 +0000

    first try

---
+++ 0x03-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include'holberton.h'
+/**
+ *
+ *
+ *
+ *
+ */
+void print_line(int n)
+{      
+  char l
+ 
+    if (n > 0 )
+      {
+       
+      (l = 0 ; l < n ; l++)
+     _putchar(_);
+      _putchar('\n');
+      }
+
+}
