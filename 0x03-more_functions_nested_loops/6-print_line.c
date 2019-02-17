26ff6b675ff2bad1e558422186cf25284efb7fa4
Jdavp <Jdavp>
2019-02-17 16:23:12 +0000

    after betty

--- 0x03-more_functions_nested_loops/6-print_line.c
+++ 0x03-more_functions_nested_loops/6-print_line.c
@@ -1,21 +1,22 @@
 #include"holberton.h"
 /**
- *
- *
- *
- *
- */
+*print_line - will print the lines declare by the user
+*
+*print number of lines
+*
+*@n: number of lines enter by the user
+*
+*/
+
 void print_line(int n)
-{      
-  int l;
- 
-    if (n > 0 )
-      {
-       
-	for (l = 0 ; l < n ; l++)
-	  _putchar('_');
-      }
-	_putchar('\n');
-   
+{
+int l;
+
+if (n > 0)
+{
+for (l = 0 ; l < n ; l++)
+_putchar('_');
+}
+_putchar('\n');
 
 }
