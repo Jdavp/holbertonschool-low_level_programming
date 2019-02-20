6edf8c32f328bc5d1e3fc7080d1935ff56b78c73
Jdavp <jdanovi@gmail.com>
2019-02-20 03:50:05 +0000

    first 3

---
+++ 0x04-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ *_puts - print string
+ *
+ *prints string
+ *
+ *@str: string pointer
+ *
+ *Return: return string length
+ **/
+
+void _puts(char *str)
+{
+
+while (*str != '\0')
+{
+_putchar(*str);
+str++;
+}
+}
