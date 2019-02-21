e643a98f4028a007db68a52d899a7b11feb0b0a2
Jdavp <jdanovi@gmail.com>
2019-02-21 00:10:48 +0000

    first 5

---
+++ 0x04-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+/**
+ *rev_string - print string in reverse after exchange positions
+ *
+ *prints string from the last point to the first exchanging positions
+ *
+ *@s: string pointer
+ *
+ *Return: return string length
+ **/
+
+void rev_string(char *s)
+{
+int front = 0;
+int back = 0;
+int aux = 0;
+
+while (s[back])
+back++;
+back--;
+
+while (front != back)
+{
+aux = s[front];
+s[front] = s[back];
+s[back] = aux;
+front++;
+back--;
+}
+}
