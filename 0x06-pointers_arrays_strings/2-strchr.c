7fc490d925ca8fe470e485b20f11b17c5dcd7580
Jdavp <jdanovi@gmail.com>
2019-02-25 22:34:58 +0000

    first 2

---
+++ 0x06-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+ *_strchr - returns a pointer after finding a similar character
+ *
+ *returns a pointer after comparing
+ *
+ *@c: compare
+ *@s: string
+ *
+ *Return: returns what its left of a string after comparing
+ **/
+
+char *_strchr(char *s, char c)
+{
+	int p = 0;
+
+	while (s[p])
+	{
+		if (s[p] == c)
+			return (&s[p]);
+		s++;
+	}
+	return (0);
+}
