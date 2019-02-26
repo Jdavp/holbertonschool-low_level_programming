d70e10a439c98845d92038ac39d578ecd3bdebbc
Jdavp <jdanovi@gmail.com>
2019-02-26 05:14:39 +0000

    first 9

---
+++ 0x06-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ *set_string - sets the value of a pointer to a char
+ *
+ *change the value of a pointer
+ *
+ *@to: string to compare
+ *@s:inicial string
+ *
+ *Return: returns what its left of a string after comparing
+ **/
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
