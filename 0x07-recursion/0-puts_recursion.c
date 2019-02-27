17057ec460c0e296b151293ee3f8a2ac8d3fe9a4
Jdavp <jdanovi@gmail.com>
2019-02-27 21:43:56 +0000

    first 0

---
+++ 0x07-recursion/0-puts_recursion.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ *
+ *
+ *_puts_recursion: prints characters after moving pointer
+ *
+ *
+ **/
+
+void _puts_recursion(char *s)
+
+
+{
+	if (*s != '\0')
+	{
+		_putchar (*s);
+		_puts_recursion(s + 1);
+	}
+	else
+			_putchar ('\n');
+
+}
