c4716ea7f733a2bd682e923acd4ecb33b6fb8ba2
Jdavp <jdanovi@gmail.com>
2019-02-27 21:41:16 +0000

    first 0

---
+++ 0x07-recursion/holberton.h
@@ -0,0 +1,16 @@
+int _putchar(char c);
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);int _putchar(char c);
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+void _puts(char *str);
+void print_rev(char *s);
+void rev_string(char *s);
+void puts2(char *str);int _putchar(char c);
+void _puts_recursion(char *s);
