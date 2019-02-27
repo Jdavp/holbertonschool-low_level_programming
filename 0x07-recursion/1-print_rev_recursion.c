991ac8ead2d5ad9e103f7edaac9ca9539f9fe7d7
Jdavp <jdanovi@gmail.com>
2019-02-27 21:59:52 +0000

    first 1

---
+++ 0x07-recursion/1-print_rev_recursion.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ *_print_rev_recursion - prints characters after moving pointer in reverse
+ *
+ *prints characters from a string in reverse
+ *
+ *@s: string
+ *
+ **/
+
+void _print_rev_recursion(char *s)
+
+{
+	if (*s != '\0')
+	{
+		_print_rev_recursion(s + 1);
+		_putchar (*s);
+	}
+
+}
