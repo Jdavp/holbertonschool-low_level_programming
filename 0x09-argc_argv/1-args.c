11323fc02f31e3a266d899635b4871cd71fe3df7
Jdavp <juandiegoalejandro@hotmail.com>
2019-03-03 18:26:41 +0000

    first 1

---
+++ 0x09-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *main - print index in a string
+ *
+ *@argc:counter
+ *@argv:vector of index
+ *
+ *Return: zero
+ **/
+
+int main(int argc, char **argv)
+{
+	(void)argv;
+		printf("%d\n", argc - 1);
+		return (0);
+}
