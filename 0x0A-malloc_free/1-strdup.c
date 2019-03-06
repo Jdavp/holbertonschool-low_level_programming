35e3079755e83c1f484acb8cac9b3832a54af36d
Jdavp <jdanovi@gmail.com>
2019-03-06 02:12:09 +0000

    first 1

---
+++ 0x0A-malloc_free/1-strdup.c
@@ -0,0 +1,36 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *_strdup - returns a pointer which contains a copy of other string
+ *
+ *@str: inicial string
+ *
+ *
+ *Return: zero
+ **/
+
+char *_strdup(char *str)
+{
+	char *sec;
+	int i;
+	int si = 0;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; str[i] != '\0'; i++)
+
+	sec = malloc(sizeof(char) * i);
+
+
+	while (str[si])
+	{
+		sec[si] = str[si];
+		si++;
+	}
+	return (sec);
+}
