72fedcc972950da8d45bb26584667c628cff58a8
Jdavp <jdanovi@gmail.com>
2019-03-06 00:06:41 +0000

    after fix order

--- 0x0A-malloc_free/0-create_array.c
+++ 0x0A-malloc_free/0-create_array.c
@@ -23,13 +23,14 @@ char *create_array(unsigned int size, char c)
 
 	a = malloc(sizeof(char) * size);
 
-	for (i = 0; i < size; i++)
-	{
-		a[i] = c;
-	}
 	if (a == NULL)
 	{
 		return (NULL);
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		a[i] = c;
 	}
 	return (a);
 }
