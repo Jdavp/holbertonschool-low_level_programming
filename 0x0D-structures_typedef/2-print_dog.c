f122d1cb6c3f7941d6d85fe9854afd891d1f6ad4
Jdavp <jdanovi@gmail.com>
2019-03-12 03:07:36 +0000

    after betty

--- 0x0D-structures_typedef/2-print_dog.c
+++ 0x0D-structures_typedef/2-print_dog.c
@@ -11,9 +11,9 @@ void print_dog(struct dog *d)
 	if (d != NULL)
 	{
 		if (d->name == NULL)
-			printf("%p\n", d->name);
+			printf("Name: %p\n", d->name);
 		if (d->owner == NULL)
-			printf("%p\n", d->owner);
+			printf("Owner: %p\n", d->owner);
 		else
 		printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 	}
