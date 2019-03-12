8f5bc7519ac1f2fbbf62f379350e0c42ee81b534
Jdavp <juandiegoalejandro@hotmail.com>
2019-03-12 05:23:07 +0000

    first 4

---
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "dog.h"
+/**
+ *print_dog - the mascot
+ *@d: user
+ */
+
+struct dog_t *new_dog(char *name, float age, char *owner)
+{
+	user = malloc(sizeof(struct dog_t));
+
+	if (new_dog == NULL)
+		return (NULL);
+	{
+	if (name == NULL);
+	{
+		return(NULL);
+		free(name);
+	}
+	if (owner == NULL);
+	{
+		return(NULL);
+		free(owner);
+	}
+	new_dog->name = name;
+	new_dog->age = age;
+	new_dog->owner = owner;
+	}
+}
