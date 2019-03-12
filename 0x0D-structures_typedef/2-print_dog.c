9cab3e79d6ca9669ad5cea23e4458dc47548ecff
Jdavp <jdanovi@gmail.com>
2019-03-12 03:01:41 +0000

    after betty

--- 0x0D-structures_typedef/2-print_dog.c
+++ 0x0D-structures_typedef/2-print_dog.c
@@ -2,21 +2,18 @@
 #include <stdio.h>
 #include "dog.h"
 /**
- *init_dog - the mascot
- *@name: name of the dog
- *@age: age of the dog
- *@owner: owner of the dog
+ *print_dog - the mascot
  *@d: user
  */
 
 void print_dog(struct dog *d)
 {
-	if(d != NULL)
-   	{
+	if (d != NULL)
+	{
 		if (d->name == NULL)
 			printf("%p\n", d->name);
 		if (d->owner == NULL)
 			printf("%p\n", d->owner);
-		printf("%s\n%f\n%s\n", d->name, d->age, d->owner);
+		printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 	}
 }
