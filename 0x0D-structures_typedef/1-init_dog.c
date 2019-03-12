82baf9458f08788eec926e62dabf74100bc838cd
Jdavp <jdanovi@gmail.com>
2019-03-12 01:32:35 +0000

    first 1

---
+++ 0x0D-structures_typedef/1-init_dog.c
@@ -0,0 +1,19 @@
+#include <stlib.h>
+#include "dog.h"
+/**
+ *init_dog - the mascot
+ *@name: name of the dog
+ *@age: age of the dog
+ *@owner: owner of the dog
+ *@d: user
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+struct dog = d;
+if (d != NULL)
+	return (NULL);
+d->name = name;
+d->age = age;
+d->owner = owner;
+}
