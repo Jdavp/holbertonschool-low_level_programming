29facc26596eee228069d784c72a8b1e6635aa41
Jdavp <jdanovi@gmail.com>
2019-03-12 00:55:15 +0000

    first dog

---
+++ 0x0D-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG
+#define DOG
+/**
+ *struct dog - the mascot
+ *@name: name of the dog
+ *@age: age of the dog
+ *@owner: owner of the dog
+ */
+
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+int _putchar(char c);
+#endif /* DOG */
