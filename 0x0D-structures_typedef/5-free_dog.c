ed12c711fceacefb871238bebcf0fdccd3f79787
Jdavp <jdanovi@gmail.com>
2019-03-12 03:39:59 +0000

    first 5

---
+++ 0x0D-structures_typedef/5-free_dog.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "dog.h"
+/**
+ *print_dog - the mascot
+ *@d: user
+ */
+
+void free_dog(dog_t *d);
+{
+	free(dog_t->name);
+	free(dog_t->age);
+	free(dog_t->owner);
+	free(d);
+	free(dog_t);
+}
