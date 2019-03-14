93c1482a485ac2f1915e1f02bc316f0f1dd12f62
Jdavp <jdanovi@gmail.com>
2019-03-14 02:38:45 +0000

    first 0

--- 0x0E-function_pointers/function_pointers.h
+++ 0x0E-function_pointers/function_pointers.h
@@ -2,6 +2,5 @@
 #define FUNCTION_POINTERS_H
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
-*
-*
-#endif /*FUNTION POINTERS*/
+void array_iterator(int *array, size_t size, void (*action)(int));
+#endif
