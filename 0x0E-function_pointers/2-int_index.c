02116d0e5e5087b539ab4d3232ed1ccd2235c886
Jdavp <jdanovi@gmail.com>
2019-03-14 04:26:13 +0000

    first 2

---
+++ 0x0E-function_pointers/2-int_index.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+ *array_iterator - iterated in a array to print
+ *@array: pointer to array
+ *@size: time it repeats
+ *@action: function that prints
+ *@
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i = 0;
+
+	while (i < size )
+	{
+
+		if (cmp)(array[i] != 0)
+		{
+			return (i);
+			i++;
+		}
+	}
+	return(-1);
+}
