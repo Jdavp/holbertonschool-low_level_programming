d6a2ed06b4ed03a5732034bc97ffb4eccb5400b9
Jdavp <jdanovi@gmail.com>
2019-03-16 20:49:49 +0000

    new dentation

--- 0x0F-variadic_functions/1-print_numbers.c
+++ 0x0F-variadic_functions/1-print_numbers.c
@@ -17,16 +17,16 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	va_start(list, n);
 
-	if (n == 0)
-		printf("\n");
-
-	while (i < n - 1)
+	if (n != 0)
+	{
+		while (i < n - 1)
 		{
-		printf("%d", va_arg(list, int));
-		if (separator != NULL)
-		printf("%s", separator);
-		i++;
+			printf("%d", va_arg(list, int));
+			if (separator != NULL)
+				printf("%s", separator);
+			i++;
 		}
+	}
 	printf("%d", va_arg(list, int));
 	printf("\n");
 	va_end(list);
