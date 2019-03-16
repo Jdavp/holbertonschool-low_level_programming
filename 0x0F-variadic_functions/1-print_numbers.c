99be9bd0a8fbb3537ea81b2d44b2a038042b4e71
Jdavp <jdanovi@gmail.com>
2019-03-16 16:27:52 +0000

    first 1

---
+++ 0x0F-variadic_functions/1-print_numbers.c
@@ -0,0 +1,29 @@
+#include <stdarg.h>
+#include "variadic_functions.h"
+#include <stdio.h>
+
+/**
+*print_numbers - print numbers
+*@n: number of parameters in the list
+*@separator: , to separate
+*
+*
+*Return: total fo the sum
+*/
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list list;
+	unsigned int i = 0;
+
+	va_start(list, n);
+
+	while (i < n - 1)
+		{
+		printf("%d", va_arg(list, int));
+		printf("%s", separator);
+		i++;
+		}
+	printf("%d", va_arg(list, int));
+	printf("\n");
+	va_end(list);
+}
