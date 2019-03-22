146be4dd6191be3c03a5657f6ddec44abee90674
Jdavp <juandiegoalejandro@hotmail.com>
2019-03-22 02:42:17 +0000

    first 0

--- 0x11-singly_linked_lists/0-print_list.c
+++ 0x11-singly_linked_lists/0-print_list.c
@@ -22,8 +22,8 @@ size_t print_list(const list_t *h)
 	}
 	while (h != NULL)
 		{
-			if (h->str == NULL)
-				/*{
+			/*if (h->str == NULL)
+				{
 				printf("[0] %p\n", h->str);
 				h = h->next;
 				i++;
