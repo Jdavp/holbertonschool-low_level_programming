951b08e88ad8be5ac016fb581fd2043fda4733a2
Jdavp <juandiegoalejandro@hotmail.com>
2019-03-22 02:32:29 +0000

    first 0

---
+++ 0x11-singly_linked_lists/0-print_list.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ *print_list - print data in a node
+ *@h: initial pointer
+ *
+ *
+ *Return: counter of nodes
+ */
+
+
+size_t print_list(const list_t *h)
+{
+	int i = 0;
+
+	if (h->str == NULL)
+	{printf("[0]%p\n", h->str);
+	h = h->next;
+	i++;
+	}
+	while (h != NULL)
+		{printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
