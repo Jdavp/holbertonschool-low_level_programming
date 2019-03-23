49ef726b4d8ce504e09660e27751ee70ae816054
Jdavp <juandiegoalejandro@hotmail.com>
2019-03-23 00:15:32 +0000

    first 2

---
+++ 0x11-singly_linked_lists/2-add_node.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include "lists.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ *add_node - add a new node
+ *@head: initial pointer
+ *@str: original string value
+ *
+ *Return: pointer of new node
+ */
+
+
+list_t *add_node(list_t **head, const char *str)
+{
+	int i = 0;
+	list_t *new = malloc(sizeof(list_t));
+	(*new).str = strdup(str);
+	while (str[i])
+	{
+		i++;
+	}
+	(*new).len = i;
+	(*new).next = *head;
+	*head = new;
+
+	return (new);
+}
