ad62e73f30cd7902ff9d7ff21ce3747082c378e2
Jdavp <juandiegoalejandro@hotmail.com>
2019-03-23 03:14:15 +0000

    try to fix

---
+++ 0x11-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include "lists.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ *add_node_end - add a new node at the end
+ *@head: initial pointer
+ *@str: original string value
+ *
+ *Return: pointer of new node
+ */
+
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *temp = *head;
+        list_t *new = malloc(sizeof(list_t));
+
+
+	
+	if (new == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+	new->str = strdup(str);
+	new->len = strlen(new->str);
+	new->next = NULL;
+	temp->next = new;
+	if (*head == NULL)
+		*head = new;
+	while(temp->next)
+		temp = temp->next;
+	temp->next = new;
+	return (new);
+
+}
