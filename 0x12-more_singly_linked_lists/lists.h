890e99f752e598d1ffc5acb6b052fc06a08e9969
Jdavp <juandiegoalejandro@hotmail.com>
2019-03-27 00:22:58 +0000

    after betty

--- 0x12-more_singly_linked_lists/lists.h
+++ 0x12-more_singly_linked_lists/lists.h
@@ -3,15 +3,6 @@
 int _putchar(char c);
 
 /**
- * struct list_s - singly linked list
- * @str: string - (malloc'ed string)
- * @len: length of the string
- * @next: points to the next node
- *
- * Description: singly linked list node structure
- * for Holberton project
- */
-/**
  * struct listint_s - singly linked list
  * @n: integer
  * @next: points to the next node
