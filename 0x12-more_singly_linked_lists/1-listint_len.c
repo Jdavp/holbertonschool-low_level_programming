24f508344cf744e5d64d3f6de84aa51b73c97594
Jdavp <juandiegoalejandro@hotmail.com>
2019-03-27 00:45:29 +0000

    first 1

---
+++ 0x12-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ *print_listint - print data in a node
+ *@h: initial pointer
+ *
+ *
+ *Return: counter of nodes
+ */
+
+
+size_t listint_len(const listint_t *h)
+{
+
+	while (h != NULL)
+		return( 1 + listint_len((*h).next));
+	return (0);
+}
