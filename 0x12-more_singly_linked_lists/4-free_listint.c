e93187eb5a3fb10c6d4af9d8364161ff4692e9d0
Jdavp <juandiegoalejandro@hotmail.com>
2019-03-28 00:47:07 +0000

    first 4

---
+++ 0x12-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ *free_listint - add a news nodes at the end
+ *@head: initial pointer
+ *
+ *
+ *Return: counter of nodes
+ */
+
+
+void free_listint(listint_t *head)
+{
+	if (!head)
+	{
+		free_listint(head->next);
+		free(head);
+	}
+}
