643cc4fe3a0b073216eeec48334e5154b99fc4a9
Jdavp <juandiegoalejandro@hotmail.com>
2019-03-29 03:31:35 +0000

    Library

--- 0x13-bit_manipulation/holberton.h
+++ 0x13-bit_manipulation/holberton.h
@@ -1,6 +1,8 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
+#include <limits.h>
+
 int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
-
+int get_bit(unsigned long int n, unsigned int index);
 #endif
