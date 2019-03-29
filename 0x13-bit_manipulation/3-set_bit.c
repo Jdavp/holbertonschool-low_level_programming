a70c407097b3d093d65395f64b6938ca1f885b27
Jdavp <juandiegoalejandro@hotmail.com>
2019-03-29 03:45:01 +0000

    first 3

---
+++ 0x13-bit_manipulation/3-set_bit.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * set_bit - set a different bit in index
+ * @n: number from system
+ * @index: index to get the bit
+ *
+ *Return: the bit
+ */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int it = 1;
+
+	if (index > 64)
+		return (-1);
+	it = it << index;
+	*n = *n | it;
+	return (*n);
+
+}
