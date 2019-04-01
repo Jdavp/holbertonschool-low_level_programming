20bba9e5a2bf2283fdddd4f7431eb6b5073dead0
Jdavp <juandiegoalejandro@hotmail.com>
2019-04-01 21:01:02 +0000

    after betty

---
+++ 0x03-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+/**
+ *print_square - print # as many times as size
+ *
+ *@size: number of times to be printed
+ *
+ */
+
+void print_square(int size)
+{
+	int i = 0;
+	int l = 0;
+
+	if (size < 0)
+		_putchar('\n');
+	while (l < size)
+	{
+		while (i < size)
+		{
+			_putchar('#');
+			i++;
+		}
+		l++;
+		i = 0;
+		_putchar('\n');
+	}
+}
