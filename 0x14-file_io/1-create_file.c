a5f8b93b26681eb7834a97601919ce931bcc500f
Jdavp <juandiegoalejandro@hotmail.com>
2019-04-04 00:21:08 +0000

    first 1

---
+++ 0x14-file_io/1-create_file.c
@@ -0,0 +1,43 @@
+#include <fcntl.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include "holberton.h"
+/**
+ *create_file - create a file with a specific content
+ *@filename: text
+ *@text_content: size of text
+ *
+ *Return:letters read and printed
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int op = 0;
+	int ite = 0;
+	int wr;
+
+	if (!filename)
+		return (-1);
+
+	op = open(filename, O_CREAT, S_IRUSR | S_IWUSR);
+	if (op == -1)
+		return (-1);
+
+	if (text_content == NULL)
+	{
+		close(op);
+		return (1);
+	}
+
+	while (text_content[ite])
+		ite++;
+
+	wr = write(op, text_content, ite);
+	if (wr == -1)
+		return (-1);
+
+	if (op == 1 && wr == 1)
+		close(op && wr);
+		return (1);
+}
