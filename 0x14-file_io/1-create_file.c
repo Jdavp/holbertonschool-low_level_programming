629dd40f62f6fa9a21ceb6db97536d68dfdd2db6
Jdavp <juandiegoalejandro@hotmail.com>
2019-04-04 00:34:24 +0000

    after new reading permits

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -20,14 +20,14 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 
-	op = open(filename, O_CREAT, S_IRUSR | S_IWUSR);
+	op = open(filename, O_CREAT | O_WRONLY, S_IRUSR | S_IWUSR);
 	if (op == -1)
 		return (-1);
 
 	if (text_content == NULL)
 	{
 		close(op);
-		return (1);
+		return (-1);
 	}
 
 	while (text_content[ite])
@@ -35,9 +35,10 @@ int create_file(const char *filename, char *text_content)
 
 	wr = write(op, text_content, ite);
 	if (wr == -1)
+	{
+		close(op);
 		return (-1);
-
-	if (op == 1 && wr == 1)
-		close(op && wr);
-		return (1);
+	}
+	close(op);
+	return (1);
 }
