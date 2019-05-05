1dc4ec8f888d3e43d17ecd16405a5e3a76a7c8f3
Jdavp <juandiegoalejandro@hotmail.com>
2019-05-05 23:39:26 +0000

    first 6

---
+++ 0x16-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ *sum_dlistint - sum of list
+ *@head: initial pointer
+ *Return: index of node
+ */
+
+int sum_dlistint(dlistint_t *head)
+{
+	int add = 0;
+	while (head != NULL)
+	{
+		add += (*head).n;
+		head = head->next;
+	}
+	return (add);
+}
