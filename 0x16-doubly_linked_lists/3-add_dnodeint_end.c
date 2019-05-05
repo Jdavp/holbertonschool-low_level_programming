f6e4c2eeb61677863b9998da5637cfd1da48e594
Jdavp <juandiegoalejandro@hotmail.com>
2019-05-05 22:12:33 +0000

    first 3

---
+++ 0x16-doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ *add_dnodeint_end - add a news nodes at the end
+ *@head: initial pointer
+ *@n:numbers
+ *
+ *Return: counter of nodes
+ */
+
+
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *temp = *head;
+	dlistint_t *new = malloc(sizeof(dlistint_t));
+
+	if (new == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+	(*new).n = n;
+	(*new).next = NULL;
+	(*new).prev = temp;
+
+	if (*head != NULL)
+	{
+		while ((*temp).next != NULL)
+			temp = temp->next;
+		(*temp).next = new;
+	}
+	else
+	{
+		*head = new;
+	}
+	return (new);
+}
