cfaa10a61998094795483d0f758b3715ce9e5174
Jdavp <juandiegoalejandro@hotmail.com>
2019-05-05 22:15:07 +0000

    add null if head null

--- 0x16-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x16-doubly_linked_lists/3-add_dnodeint_end.c
@@ -16,6 +16,8 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	dlistint_t *temp = *head;
 	dlistint_t *new = malloc(sizeof(dlistint_t));
 
+	if (head == NULL)
+		return (NULL);
 	if (new == NULL)
 	{
 		free(new);
