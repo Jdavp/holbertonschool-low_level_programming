b417c5dbe9182327c24d0699eec567b8d0f59b42
Jdavp <juandiegoalejandro@hotmail.com>
2019-05-06 04:34:01 +0000

    various fixes

--- 0x16-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x16-doubly_linked_lists/7-insert_dnodeint.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 
 /**
- *add_dnodeint - add a news nodes to the begin
+ *insert_dnodeint_at_index - add a news node at given index
  *@h: initial pointer
  *@n:numbers
  *@idx: index
@@ -11,22 +11,20 @@
  */
 
 
-dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	unsigned int it = 0;
 	dlistint_t *gotnode;
-	dlistint_t *new;
+	dlistint_t *new = NULL;
 
 	if (idx == 0)
 		return (add_dnodeint(h, n));
 
 	gotnode = get_dnodeint_at_index(*h, idx);
 
-	if (gotnode != NULL && (*(gotnode).next = NULL)
-		{return (add_dnodeint_end(h, idx));
-		}
+	if (gotnode != NULL && (*gotnode).next == NULL)
+		return (add_dnodeint_end(h, idx));
 
-	if (idx != 0 && (*(gotnode).next != NULL))
+	if (idx != 0 && (*gotnode).next != NULL)
 		new = malloc(sizeof(dlistint_t));
 
 	if (new == NULL)
@@ -35,15 +33,13 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
 		return (NULL);
 	}
 
-	if (*h != NULL)
-		(*h)->prev = new;
-
+	(*new).prev = (*gotnode).prev;
+	(*(*gotnode).prev).next = new;
 	(*new).n = n;
-	(*new).next = next;
-	(*new).prev = prev;
-	*h = new;
+	(*new).next = gotnode;
+		(*gotnode).prev = new;
 
-	return (new);
+		return (new);
 }
 /**
  *get_dnodeint_at_index - return the nth node of a list
