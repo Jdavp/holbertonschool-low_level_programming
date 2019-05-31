be4601196b1d88a40761164b2feb6005afa63f18
Juan Diego Alejandro Valencia Pena <juandiegoalejandro@hotmail.com>
2019-05-31 20:03:13 +0000

    first 0

--- 0x19-hash_tables/0-hash_table_create.c
+++ 0x19-hash_tables/0-hash_table_create.c
@@ -2,15 +2,25 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
-#define HASHTABLE_SIZE 1024
 /**
- *
- *
- *
+ *hash_table_create - function to create a hashtable
+ *@size: size of the array that determines hashtables lenght
+ *Return: address to the array
  */
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *hashtable = malloc(HASHTABLE_SIZE * sizeof(size));
-	return hashtable;
+	hash_table_t *hashtable = malloc(sizeof(hash_table_t));
+
+	if (hashtable == NULL)
+	{
+	return (NULL);
+	}
+	hashtable->size = size;
+	hashtable->array = calloc(size, sizeof(hash_node_t));
+	if (hashtable->array == NULL)
+	{
+	return (NULL);
+	}
+	return (hashtable);
 }
