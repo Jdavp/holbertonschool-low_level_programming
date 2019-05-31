920273685895fbf2f542757befe921bbb42594ed
Juan Diego Alejandro Valencia Pena <juandiegoalejandro@hotmail.com>
2019-05-31 23:51:05 +0000

    first 3

---
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -0,0 +1,45 @@
+#include "hash_tables.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+/**
+ *key_index - function that gives you the index of a key
+ *@size: size of the array that determines hashtables lenght
+ *@key: string key in the node
+ *Return: address to the array
+ */
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	const unsigned char *newkey;
+	unsigned long int keyindex;
+	hash_node_t *newnode;
+	hash_node_t *auxposition;
+
+	if(!ht || !strlen(key) || !ht ->size)
+		return(0);
+
+
+	newkey = (const unsigned char *)key;
+	keyindex = key_index(newkey,ht->size);
+       	auxposition = ht->array[keyindex];
+	if(ht->array[keyindex] != NULL)
+	{
+		while(ht->array[keyindex] != NULL)
+		{
+			if (strcmp (ht->array[keyindex]->key, key) == 0)
+			{
+				ht->array[keyindex]->value = strdup(value);
+				return(1);
+			}
+			auxposition = ht->array[keyindex]->next;
+		}
+	}
+	newnode = malloc(sizeof(hash_node_t));
+	newnode->key = strdup(key);
+	newnode->value = strdup(value);
+	newnode->next = auxposition;
+	(*ht).array[keyindex] = newnode;
+
+	return(1);
+}
