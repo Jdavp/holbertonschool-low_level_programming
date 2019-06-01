4bf7a0dde6f6c9815766609ecba6538ee6b34834
Juan Diego Alejandro Valencia Pena <juandiegoalejandro@hotmail.com>
2019-06-01 02:54:35 +0000

    first 5

---
+++ 0x19-hash_tables/5-hash_table_print.c
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "hash_tables.h"
+
+/**
+ *hash_table_print - function that add an elemet to the hash table
+ *@ht: pointer to the hash table
+ *Return: address to the array
+ */
+
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned int i = 0, m0 = 1;
+	hash_node_t **ai = ht->array;
+
+	if (ht == NULL)
+		return;
+	printf("{");
+	while (i < ht->size)
+	{
+		while (ai[i] != NULL)
+		{
+			if (m0 == 1)
+			{
+				printf("'%s':'%s'", ai[i]->key, ai[i]->value);
+				ai[i] = ai[i]->next;
+				m0 = 0;
+			}
+			else
+			{
+				printf(", '%s':'%s'", ai[i]->key, ai[i]->value);
+				ai[i] = ai[i]->next;
+			}
+		}
+	i++;
+	}
+	printf("}\n");
+}
