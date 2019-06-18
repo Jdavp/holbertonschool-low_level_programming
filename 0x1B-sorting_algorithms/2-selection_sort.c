204a3b9f650934baefeefdbd21fe9a35db5b1974
Juan Diego Alejandro Valencia Pena <juandiegoalejandro@hotmail.com>
2019-06-18 03:37:24 +0000

    first 2

---
+++ 0x1B-sorting_algorithms/2-selection_sort.c
@@ -0,0 +1,39 @@
+#include "sort.h"
+#include <stdio.h>
+
+/**
+ * bubble_sort - function that sorts an array of integers in ascending
+ * order using the Bubble sort algorithm
+ * @array: pointer to an int array
+ * @size: size of the array
+ * Return: Nothing
+ */
+void selection_sort(int *array, size_t size)
+{
+	int index = 0, min = 0;
+	size_t i = 0, counter = 0;
+
+	min = array[i];
+	while (counter < size - 1)
+	{
+		i++;
+		if (array[i])
+		{
+			if (array[i] < min)
+			{
+				min = array[i];
+				index = i;
+			}
+			continue;
+		}
+		if (array[counter] != min)
+		{
+			array[index] = array[counter];
+			array[counter] = min;
+			print_array(array, size);
+		}
+		counter++;
+		i = counter;
+		min = array[i];
+	}
+}
