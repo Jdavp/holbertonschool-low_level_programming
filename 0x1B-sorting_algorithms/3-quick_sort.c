7ab6348e651221af7c296b8e786d7178e0fa3244
RodrigoSierraV <735@holbertonschool.com>
2019-06-19 01:32:51 +0000

    Final quick-sort algorithm

--- 0x1B-sorting_algorithms/3-quick_sort.c
+++ 0x1B-sorting_algorithms/3-quick_sort.c
@@ -1,5 +1,7 @@
 #include "sort.h"
 #include <stdio.h>
+void quick_recursion(int *array, int first, int last, size_t size);
+size_t partition(int *array, int first, int last, size_t size);
 /**
  * quick_sort - function that sorts an array of integers in ascending order
  * using the Quick sort algorithm with Lomuto partition scheme.
@@ -9,13 +11,21 @@
  */
 void quick_sort(int *array, size_t size)
 {
-	size_t pivot = size - 1, j = 0;
-	int aux = 0, i = -1;
+	int first = 0, last = (size - 1);
 
+	quick_recursion(array, first, last, size);
 }
-size_t partition(int *array, int first, int last)
+/**
+ * partition - executes Lomuto partition scheme
+ * @array: pointer to an int array
+ * @first: first position of the partition
+ * @last: last position of the partition
+ * @size: size of the array
+ * Return: int with the new position of the pivot
+ */
+size_t partition(int *array, int first, int last, size_t size)
 {
-	int i = first - 1, j = first;
+	int i = first - 1, j = first, pivot = last, aux = 0;
 
 	while (j < last)
 	{
@@ -25,7 +35,8 @@ size_t partition(int *array, int first, int last)
 			aux = array[j];
 			array[j] = array[i];
 			array[i] = aux;
-			print_array(array, size);
+			if (array[i] != array[j])
+				print_array(array, size);
 		}
 		j++;
 	}
@@ -33,6 +44,26 @@ size_t partition(int *array, int first, int last)
 	aux = array[pivot];
 	array[pivot] = array[i];
 	array[i] = aux;
-	return(i);
+	if (array[pivot] != array[i])
+		print_array(array, size);
+	return (i);
 }
+/**
+ * quick_recursion - recursive function to execute partitions sort
+ * @array: pointer to an int array
+ * @first: first position of the partition
+ * @last: last position of the partition
+ * @size: size of the array
+ * Return: int with the new position of the pivot
+ */
+void quick_recursion(int *array, int first, int last, size_t size)
+{
+	int pivot;
 
+	if (first < last)
+	{
+		pivot = partition(array, first, last, size);
+		quick_recursion(array, first, pivot - 1, size);
+		quick_recursion(array, pivot + 1, last, size);
+	}
+}
